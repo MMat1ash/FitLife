eacc26b55f7502deb0aa2815283da4a0dc3324ed
Max Matiash <maks-matyash@ya.ru>
2026-04-18 16:04:21 +0300

    Исправления по автотестам на сервере

--- fit_life.py
+++ fit_life.py
@@ -1,6 +1,6 @@
 # Начало работы, получение данных от пользователья, расчеты
 print("Привет, пользователь!")
-user_name = input("Как тебя зовут? ")
+user_name = input("Как тебя зовут? ").title()
 user_age = int(input("Сколько тебе лет? "))
 weight = float(input("Какой у тебя вес (кг)? "))
 height = float(input("Какой у тебя рост (в метрах, используя точку: 1.75)? "))
@@ -16,15 +16,14 @@ border = 60
 def title_shift(text):
     """Функция для центрирования заголовка и футера."""
     title_len = len(text)
-    title_shift = (border - title_len) // 2
-    return title_shift
+    return (border - title_len) // 2
 
 
 # Вывод отчета
 print("=" * border)
 print(" " * title_shift(title) + title)
 print("=" * border)
-print(f"{user_name.title()}, возраст: {user_age}.")
+print(f"{user_name}, возраст: {user_age}.")
 print(f"Индекс массы тела (ИМТ): {bmi}.")
 print(f"Рекомендуемое суточное потребление воды (л): {water_l}.")
 print("=" * border)
