6e7076b8bbf73a953ee41c3e8842250516cfe9d8
Max Matiash <maks-matyash@ya.ru>
2026-04-19 18:33:08 +0300

    Исправлены замечания

--- fit_life.py
+++ fit_life.py
@@ -1,31 +1,76 @@
-# Начало работы, получение данных от пользователья, расчеты
-print("Привет, пользователь!")
-user_name = input("Как тебя зовут? ").title()
-user_age = int(input("Сколько тебе лет? "))
-weight = float(input("Какой у тебя вес (кг)? "))
-height = float(input("Какой у тебя рост (в метрах, используя точку: 1.75)? "))
-bmi = round((weight / (height ** 2)), 1)
-water_l = round(((weight * 30) / 1000), 1)
+"""
+Калькулятор индекса массы тела и суточной потребности воды.
 
-# Рыба для отчета:
-title = "Отчет о пользователе:"
-footer = "Отчет завершен. Будьте здоровы!"
-border = 60
+Принимает от пользователя:
+        имя (user_name), возраст (user_age), вес (weight) и рост (height).
+Расcчитывает ИМТ (bmi) и суточную потребность в воде (water_l)
+На основание полученных и расчитанных данных формирует отчет.
+"""
+
+
+WATER_PER_KG = 30
+MILLILITERS_IN_A_LITER = 1000
+TITLE = "Отчет о пользователе:"
+FOOTER = "Отчет завершен. Будьте здоровы!"
+BORDER = 60
+
+print("Привет, пользователь!")
+
+user_name = input("Как тебя зовут? ").title()
+
+while True:
+    try:
+        user_age = int(input("Сколько тебе лет? "))
+        if user_age <= 0:
+            print("Возраст должен быть больше 0!")
+            continue
+        break
+    except ValueError:
+        print("Пожалуйста, введите возраст целым числом.")
+
+while True:
+    try:
+        weight = float(input("Какой у тебя вес (кг)? "))
+        if weight <= 0:
+            print("Вес должен быть больше 0!")
+            continue
+        break
+    except ValueError:
+        print("Используйте цифры и точку. Например: 75.5")
+
+while True:
+    try:
+        height = float(input("Какой у тебя рост (в метрах)? "))
+        if height <= 0:
+            print("Рост должен быть больше 0!")
+            continue
+        break
+    except ValueError:
+        print("Используйте цифры и точку. Например: 1.75")
+
+bmi = round((weight / (height ** 2)), 1)
+water_l = round(((weight * WATER_PER_KG) / MILLILITERS_IN_A_LITER), 1)
 
 
 def title_shift(text):
-    """Функция для центрирования заголовка и футера."""
-    title_len = len(text)
-    return (border - title_len) // 2
+    """
+    Вычисляет длину отступа слева для центрирования TITLE и FOOTER.
+
+    Параметр:
+            text(str): текст для центрирования.
+
+    Возвращаемое значение:
+            int: необходимое количество пробелов для отступа слева.
+    """
+    return (BORDER - len(text)) // 2
 
 
-# Вывод отчета
-print("=" * border)
-print(" " * title_shift(title) + title)
-print("=" * border)
+print("=" * BORDER)
+print(" " * title_shift(TITLE) + TITLE)
+print("=" * BORDER)
 print(f"{user_name}, возраст: {user_age}.")
 print(f"Индекс массы тела (ИМТ): {bmi}.")
 print(f"Рекомендуемое суточное потребление воды (л): {water_l}.")
-print("=" * border)
-print(" " * title_shift(footer) + footer)
-print("=" * border)
+print("=" * BORDER)
+print(" " * title_shift(FOOTER) + FOOTER)
+print("=" * BORDER)
